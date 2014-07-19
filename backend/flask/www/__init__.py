6677a50cc3d337a5955eec5a809fa81cce65a9e6
Martin Andrews <GitHub@mdda.net>
2014-07-19 12:06:30 +0800

    Read enviroment variables for config - and tidy up

--- backend/flask/www/__init__.py
+++ backend/flask/www/__init__.py
@@ -10,21 +10,14 @@ app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 app.secret_key = 'ENTER RANDOM STUFF HERE'
 app.debug=True # Works in uwsgi environment
 
-#import platform
-#hostname = platform.uname()[1]
-
 # See http://docs.sqlalchemy.org/ru/latest/dialects/sqlite.html#connect-strings
 app.config['DATABASE'] = os.environ.get("FLASK_DB", 'sqlite:///sqlite_hackathon.db') # Relative URL
+# 'sqlite:////home/USERNAME/hackathon-starter-flask-plus/backend/flask/sqlite_hackathon.db' # Absolute URL
+
 app.config['SENDMAIL'] = os.environ.get("FLASK_MAIL", False)
 app.config['SENDMAIL_FILE'] = os.environ.get("FLASK_MAIL_FILE", 'mime_message.txt')
+
 app.debug = os.environ.get("FLASK_DEBUG", False)
-
-#if hostname == 'example.com':
-#    #app.config['DATABASE'] = 'sqlite:////home/USERNAME/hackathon-starter-flask-plus/backend/flask/sqlite_hackathon.db' # Absolute URL
-#    app.config['SENDMAIL'] = True
-#    # app.debug=False  # This appears to be safe under uwsgi - 
-
-#app.config['DATABASE'] = os.environ.get('FLASK_OVERRIDE_DB', app.config['DATABASE'])
 
 # Pull in routes
 import www.main
