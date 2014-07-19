0efd3a389f42a3813065933c569c6c1f16d5f1c9
Martin Andrews <GitHub@mdda.net>
2014-07-19 12:04:16 +0800

    Read enviroment variables for config

--- backend/flask/run.py
+++ backend/flask/run.py
@@ -4,19 +4,24 @@ HOST = 'localhost'   # This restricts incoming calls to the local machine
 #HOST = '0.0.0.0'     # This allows incoming calls from outside the machine (Windows will ask for Firewall permission)
 PORT = 7882          # Arbitrary port (epoch accessible from outside)
 
-import platform
-hostname = platform.uname()[1]
+#import platform
+#hostname = platform.uname()[1]
 
-print "HOSTNAME = %s" % hostname
+#print "HOSTNAME = %s" % hostname
 
+import os
 # Better default choice than True
-debug=False
-if hostname.endswith('herald') :
-    debug=True
+debug=os.environ.get("FLASK_DEBUG", False)
+
+#if hostname.endswith('herald') or hostname.endswith('localhost') :
+#    debug=True
 
 ## http://blog.rootsmith.ca/uncategorized/request-server-or-hostname-must-match-flasks-server_name-to-route-successfully/
 #import os
 #if 'SERVER_NAME' in os.environ:
 #    print os.environ['SERVER_NAME']
+
+if debug:
+  print "Debugging is on"
 
 app.run(host=HOST, port=PORT, debug=debug, )
